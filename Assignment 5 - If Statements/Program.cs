6ffe8b0d88bd5ec6c1656e105fd3d1d64440536a
Ashton Hotke <339741886@GoTVDSB.ca>
2024-09-17 09:13:55 -0400

    Add project files.

---
+++ Assignment 5 - If Statements/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Assignment_5___If_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double weight;
+
+            // Space Boxing
+            Console.WriteLine("Hello, Julio Cesar Chavez Mark VII!");
+            Console.Write("Please enter your wait on Earth:");
+            weight = Console.ReadLine();
+        }
+    }
+}
